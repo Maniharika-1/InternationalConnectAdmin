7b60caee1bb0910ca2bbcf273c194daf5c6ce03f
Maniharika-1 <maniharika1998@gmail.com>
2023-02-26 18:17:45 +0530

    Updated getAdminInfo() method signature and added listener to fetch data.

--- app/src/main/java/com/example/admininternationalconnect/data/repository/datasource/AdminDataSource.java
+++ app/src/main/java/com/example/admininternationalconnect/data/repository/datasource/AdminDataSource.java
@@ -1,7 +1,10 @@
 package com.example.admininternationalconnect.data.repository.datasource;
 
+import com.example.admininternationalconnect.domain.util.GetDataListener;
+import com.google.firebase.database.DatabaseReference;
+
 public interface AdminDataSource {
 
-    void getAdminInfo();
+    void getAdminInfo(DatabaseReference adminNode, final GetDataListener getDataListener);
 
 }
--- app/src/main/java/com/example/admininternationalconnect/data/repository/datasourceImpl/AdminDataSourceImpl.java
+++ app/src/main/java/com/example/admininternationalconnect/data/repository/datasourceImpl/AdminDataSourceImpl.java
@@ -1,11 +1,40 @@
 package com.example.admininternationalconnect.data.repository.datasourceImpl;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.admininternationalconnect.data.model.Admin;
 import com.example.admininternationalconnect.data.repository.datasource.AdminDataSource;
+import com.example.admininternationalconnect.domain.util.GetDataListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdminDataSourceImpl implements AdminDataSource {
 
+    private String TAG = "AdminDataSourceImpl";
+
     @Override
-    public void getAdminInfo() {
+    public void getAdminInfo(DatabaseReference adminNode, final GetDataListener getDataListener) {
+
+        getDataListener.onStart();
+        adminNode.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    Admin admin = snapshot.getValue(Admin.class);
+                    getDataListener.onSuccess(admin);
+                } else getDataListener.onFailure();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "onCancelled: " + error.getMessage());
+                getDataListener.onFailure();
+            }
+        });
 
     }
 
