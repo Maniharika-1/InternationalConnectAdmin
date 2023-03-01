824aa9f380711ef05b9977b190ac46fd9fbb4a46
Maniharika-1 <maniharika1998@gmail.com>
2023-03-01 16:46:09 +0530

    Updated LoginViewModel to extend AndroidViewModel and added code to store login status in shared preferences.

--- app/src/main/java/com/example/admininternationalconnect/presentation/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/example/admininternationalconnect/presentation/viewmodel/LoginViewModel.java
@@ -1,28 +1,37 @@
 package com.example.admininternationalconnect.presentation.viewmodel;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.admininternationalconnect.R;
 import com.example.admininternationalconnect.data.model.Admin;
 import com.example.admininternationalconnect.domain.usecase.LoginUseCase;
 import com.example.admininternationalconnect.domain.util.GetDataListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class LoginViewModel extends ViewModel {
+public class LoginViewModel extends AndroidViewModel {
 
     LoginUseCase loginUseCase;
     DatabaseReference adminNode;
     public MutableLiveData<String> errorMessage;
     Boolean loginSuccessful;
+    SharedPreferences sharedPreferences;
     final String TAG = "LoginViewModel";
-    
-    public LoginViewModel(LoginUseCase loginUseCase) {
+
+    public LoginViewModel(Application application, LoginUseCase loginUseCase) {
+        super(application);
         this.loginUseCase = loginUseCase;
         adminNode = FirebaseDatabase.getInstance().getReference().child("admin");
         errorMessage = new MutableLiveData<>();
         errorMessage.setValue(null);
         loginSuccessful = true;
+        sharedPreferences = getApplication().getSharedPreferences(getApplication().getString(R.string.shared_preferences_name), Context.MODE_PRIVATE);
     }
 
     public void login(String userName, String password) {
@@ -36,10 +45,12 @@ public class LoginViewModel extends ViewModel {
                     //call intent
                     loginSuccessful = true;
                     errorMessage.setValue(null);
+                    sharedPreferences.edit().putBoolean("LoggedIn", true).apply();
 
                 } else {
                     loginSuccessful = false;
                     errorMessage.setValue("User name or password is incorrect!");
+                    sharedPreferences.edit().putBoolean("LoggedIn", false).apply();
                 }
             }
 
@@ -51,6 +62,7 @@ public class LoginViewModel extends ViewModel {
             public void onFailure() {
                 loginSuccessful = false;
                 errorMessage.setValue("User name or password is incorrect!");
+                sharedPreferences.edit().putBoolean("LoggedIn", false).apply();
             }
         });
     }
