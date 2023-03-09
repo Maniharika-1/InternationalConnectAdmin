cc13499f4a478f47ff9950cb7752406a053b45b0
Maniharika-1 <maniharika1998@gmail.com>
2023-03-09 23:17:09 +0530

    Removed shared preference operations from LoginViewModel.

--- app/src/main/java/com/example/admininternationalconnect/presentation/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/example/admininternationalconnect/presentation/viewmodel/LoginViewModel.java
@@ -1,17 +1,16 @@
 package com.example.admininternationalconnect.presentation.viewmodel;
 
 import android.app.Application;
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.util.Log;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-import com.example.admininternationalconnect.R;
+import com.example.admininternationalconnect.AdminBaseClass;
 import com.example.admininternationalconnect.data.model.Admin;
 import com.example.admininternationalconnect.domain.usecase.LoginUseCase;
 import com.example.admininternationalconnect.domain.util.GetDataListener;
+import com.example.admininternationalconnect.presentation.util.SharedPreferencesUtil;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,8 +19,8 @@ public class LoginViewModel extends AndroidViewModel {
     LoginUseCase loginUseCase;
     DatabaseReference adminNode;
     public MutableLiveData<String> errorMessage;
-    Boolean loginSuccessful;
-    SharedPreferences sharedPreferences;
+    public MutableLiveData<Boolean> loginSuccessful;
+    SharedPreferencesUtil sharedPreferencesUtil;
     final String TAG = "LoginViewModel";
 
     public LoginViewModel(Application application, LoginUseCase loginUseCase) {
@@ -29,28 +28,27 @@ public class LoginViewModel extends AndroidViewModel {
         this.loginUseCase = loginUseCase;
         adminNode = FirebaseDatabase.getInstance().getReference().child("admin");
         errorMessage = new MutableLiveData<>();
+        loginSuccessful = new MutableLiveData<>();
         errorMessage.setValue(null);
-        loginSuccessful = true;
-        sharedPreferences = getApplication().getSharedPreferences(getApplication().getString(R.string.shared_preferences_name), Context.MODE_PRIVATE);
+        //loginSuccessful.setValue(false);
+        sharedPreferencesUtil = new SharedPreferencesUtil();
     }
 
     public void login(String userName, String password) {
-
         loginUseCase.execute(adminNode, new GetDataListener() {
             @Override
             public void onSuccess(Admin admin) {
-
                 if (admin != null && admin.getUserName() != null && admin.getUserName().contentEquals(userName)
                         && admin.getPassword() != null && admin.getPassword().contentEquals(password)) {
-                    //call intent
-                    loginSuccessful = true;
+
+                    loginSuccessful.setValue(true);
                     errorMessage.setValue(null);
-                    sharedPreferences.edit().putBoolean("LoggedIn", true).apply();
 
                 } else {
-                    loginSuccessful = false;
+
+                    loginSuccessful.setValue(false);
                     errorMessage.setValue("User name or password is incorrect!");
-                    sharedPreferences.edit().putBoolean("LoggedIn", false).apply();
+
                 }
             }
 
@@ -60,10 +58,10 @@ public class LoginViewModel extends AndroidViewModel {
 
             @Override
             public void onFailure() {
-                loginSuccessful = false;
+                loginSuccessful.setValue(false);
                 errorMessage.setValue("User name or password is incorrect!");
-                sharedPreferences.edit().putBoolean("LoggedIn", false).apply();
             }
         });
     }
+
 }
